bf3a8cb91550995cb90f751941414dc83b6ea6a5
kmordarski <kacper.mordarski@gmail.com>
2022-04-22 14:01:58 +0200

    changes on 22.04.2022

--- Julia/simulation.jl
+++ Julia/simulation.jl
@@ -1,2 +1,24 @@
-print("Hello world!")
+import Graphs
+import TikzPictures 
+using GraphPlot, Compose
+using Cairo, Fontconfig
 
+
+BAM = Graphs.SimpleGraphs.barabasi_albert(1000, 5, 2, is_directed = true)
+
+
+print(BAM)
+
+
+draw(PNG("BAM_1.png", 16cm, 16cm),gplot(BAM))
+
+
+
+# Creating Scale-Free Network in compliance with preferential attachment and growth rules. Each vertex corresponds 
+
+# Defining games that will be played throughout the population (snowdrift or prisoners dillema) in order to show the emergence of
+# cooperations 
+
+#
+
+#
