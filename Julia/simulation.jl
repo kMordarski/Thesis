ae2c6a89b213b3dfdabae3402cdc5685dd336178
kmordarski <kacper.mordarski@gmail.com>
2022-05-14 19:33:24 +0200

    14.05.2022

--- Julia/simulation.jl
+++ Julia/simulation.jl
@@ -1,5 +1,7 @@
 # Symulacje do pracy licencjackiej
 
+import Distributions
+using Pkg
 import Graphs
 using Graphs
 using Plots
@@ -8,10 +10,11 @@ using GraphPlot, Compose
 using Cairo, Fontconfig
 using Random
 using Base.Iterators: partition
+using DataStructures
 
 # Parametrisation (for the PD game)
 
-T_PD = 1.5 # This parameter is the advantage of defectors over cooperators, being typically constrained to the interval 1 < b <= 2
+T_PD = 1.1 # This parameter is the advantage of defectors over cooperators, being typically constrained to the interval 1 < b <= 2
 R_PD = 1
 P_PD = 0
 S_PD = 0
@@ -44,57 +47,8 @@ edge = collect(edges(BAM))
 
 edge_1 = edge[1]
 
-edge_1
-
-typeof(edge_1)
-
-typeof(edges)
-
-for n in edge
-    print(n)
-end
-
-function transform(e)
-
-    p = [src(e), dst(e)]
-
-end
-
-function strat(e)
-    p = [strategies[e[1]],strategies[e[2]]]
-end
-
-@time begin
-edges_new = map(transform, edge)
-end
-
-@time begin
-    edges_with_strat = map(strat, edges_new)
-end
-
-edges_new[1][2]
-
+# Distribution of the strategies
 strategies = rand([1,2], 10000)
-
-payoffs_PD[edges_with_strat[4][1], edges_with_strat[4][2]]
-
-edges_with_strat[4]
-edges_new[4]
-
-function games(x, y, V)
-    for n in 1:length(x)
-        V[y[n][1]] += payoffs_PD[x[n][1], x[n][2]][1]
-        V[y[n][2]] += payoffs_PD[x[n][1], x[n][2]][2]
-    end
-    return V
-end
-
-@time begin
-acc_payoffs_new = games(edges_with_strat, edges_new, acc_payoffs)
-end
-
-
-findmax(acc_payoffs_new)
 
 # Payoffs matrix for the PD game
 
@@ -117,50 +71,139 @@ payoffs_SG[2,2] = [P_SG, P_SG]
 
 payoffs_SG
 
+
+#Defining functions to map strategies onto edges
+
+function transform(e::Vector{Graphs.SimpleEdge{Int16}})
+
+    p = [src(e), dst(e)]
+
+end
+
+function strat(e::Vector{Vector{Int16}})
+    p = [strategies[e[1]],strategies[e[2]]]
+end
+
+@time begin
+edges_new = map(transform, edge)
+end
+
+@time begin
+    edges_with_strat = map(strat, edges_new)
+end
+
+payoffs_PD[edges_with_strat[4][1], edges_with_strat[4][2]]
+
+typeof(edges_new)
+
+function games(x::Vector{Vector{Int64}}, y::Vector{Vector{Int8}}, V::Matrix{Float64}) # V is an array of cummulated payoffs, x is an array of edges, y is an array of strategies
+    for n in 1:length(x)
+        V[y[n][1]] += payoffs_PD[x[n][1], x[n][2]][1]
+        V[y[n][2]] += payoffs_PD[x[n][1], x[n][2]][2]
+    end
+    return V
+end
+
+@time begin
+acc_payoffs_new = games(edges_with_strat, edges_new, acc_payoffs)
+end
+
 # Defining a function to look for a neighbor with different strategy than agent selected
 
-function check_strat(a, all_strats, BAM)
+n = neighbors(BAM, 100)
+
+function check_strat(a::Int64, y::Vector{Int64}, BAM::SimpleGraph{Int16}) # a is a randomly chosen vertex, y is an array of strategies, BAM is a Barabassi-Albert network, V is an array of cummulated payoffs
 
     neigh = shuffle(neighbors(BAM, a))
-
-    for i in neigh
-        if get(all_strats, a, 0) != get(all_strats, i, 0)    
-            
-            global b = i
-
+    for n in neigh
+    if y[a] != y[n]
+            global b = n
             break
         end
     end
-    print(b)
-    return(b)
-end
+    
+    if G == "PD"
+        D = T_PD
+    elseif G == "SG"
+        D = T_SG
+    end
 
-check_strat(100, all_strats, BAM)
+    k = max(length(neighbors(BAM, a)), length(neighbors(BAM, b)))
+
+    if acc_payoffs[a] > acc_payoffs[b]
+        prob = (acc_payoffs[a] - acc_payoffs[b])/D*k
+        if rand() <= prob
+            y[b] = y[a]
+        end
+    elseif acc_payoffs[a] < acc_payoffs[b]
+        prob = (acc_payoffs[b] - acc_payoffs[a])/D*k
+        if rand() <= prob
+            y[a] = y[b]
+        end
+    end
+    return b
+end
+# defining which game is to be played
+
+G = "PD"
+
+A = [1,2,3,4,5]
+B = [2,3,4,5,6]
+D = [3,4,5,6,7]
+
+C = [A,B]
+
+C = push!(C, D)
+
 
 # Loop that performs simulations
+@time begin
+#    for j in 1:100 # This loop controlles building new NF SOC'sq and mapping their edges to define the stratgies
 
-for j in 1:100 # This loop controlles building new NF SOC's
+        BAM = Graphs.SimpleGraphs.barabasi_albert(N, m0, m0, is_directed = false) # Generating SF NOC
 
-    BAM = Graphs.SimpleGraphs.barabasi_albert(N, m0, m0, is_directed = false) # Generating SF NOC
+        strategies = rand([1,2], N)
 
-    # This loop is responsible for evaluating results of games
+        all_edges = collect(edges(BAM))
 
-    for k in 1:11000
-    
-    end
+        all_edges_final = map(transform, all_edges)
+
+        all_edges_strats = map(strat, all_edges_final)
+        
+        #if j == 1
+        #    global Arr_paths = [track]
+        #elseif j > 1
+        #    Arr_paths = push!(Arr_paths, track)
+        #end
+
+        global track = Vector{Float64}() # Keeping track of the coop/def proportion
+
+        # This loop is responsible for evaluating results of games
+
+        for k in 1:11000
+
+            acc_payoffs_new = games(edges_with_strat, edges_new, acc_payoffs)
+            
+            a = rand(1:10000)
+
+            check_strat(a, strategies, BAM)
+
+            if k > 10000
+                c_d = counter(strategies)[1]/(counter(strategies)[2] + counter(strategies)[1])
+
+                track = append!(track, c_d)
+            end
+        end
+#    end
 end
+
+track
 
 rand(1:10000)
 
-rand(all_strats)
-
-values(all_strats)
+c = counter(strategies)[1]/(counter(strategies)[2] + counter(strategies)[1])
 
 # Creating Scale-Free Network in compliance with preferential attachment and growth rules. Each vertex corresponds 
 
 # Defining games that will be played throughout the population (snowdrift or prisoners dillema) in order to show the emergence of
 # cooperations 
-
-#
-
-#
